25c4ab91d583860819ed09982fb07c53d0e57d0e
wildersonh <wildersonhernandez994@gmail.com>
2025-04-26 23:19:08 -0400

    mod n° 2 colores

--- script.js
+++ script.js
@@ -61,18 +61,6 @@ function checkBoxes() {
 }
 
 
-btnSwitch.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
-    btnSwitch.classList.toggle("active");
-
-    //guardar modo
-
-    if (document.body.classList.contains("dark")) {
-        localStorage.setItem("dark-mode", "true");
-    } else{
-        localStorage.setItem("dark-mode", "false");
-    }
-})
 
 
 //optener modo al actualizar
