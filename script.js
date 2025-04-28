c6651bd8ceb249bb797103e43efc37a8feb45d2d
wildersonh <wildersonhernandez994@gmail.com>
2025-04-27 23:12:56 -0400

    mod del nav

--- script.js
+++ script.js
@@ -61,6 +61,18 @@ function checkBoxes() {
 }
 
 
+btnSwitch.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    btnSwitch.classList.toggle("active");
+
+    //guardar modo
+
+    if (document.body.classList.contains("dark")) {
+        localStorage.setItem("dark-mode", "true");
+    } else{
+        localStorage.setItem("dark-mode", "false");
+    }
+})
 
 
 //optener modo al actualizar
@@ -71,3 +83,4 @@ if(localStorage.getItem("dark-mode") === "true"){
 } else {
     document.body.classList.remove("dark");
 }
+
